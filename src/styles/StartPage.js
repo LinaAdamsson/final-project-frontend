4bd1db2071e87bf46b3e8805a909ca39d67db86b
andrea hedström <andrea.hedstrom@outlook.com>
2023-06-12 11:27:06 +0200

    margins for intropage

--- src/styles/StartPage.js
+++ src/styles/StartPage.js
@@ -3,7 +3,7 @@ import styled from 'styled-components/macro';
 export const IntroText = styled.div`
 display: flex;
 justify-content: center;
-margin: 200px 0 -210px 0;
+margin: 50px 0 -110px 0;
 h2{
 font-size: 20px;
 color: white;
