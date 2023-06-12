c20134a14bd7bc06a306d004cd8467e3284ed1d7
andrea hedström <andrea.hedstrom@outlook.com>
2023-06-12 11:47:16 +0200

    space

--- src/components/MissionBoard.js
+++ src/components/MissionBoard.js
@@ -160,7 +160,7 @@ const MissionBoard = () => {
                 </PopupModal>
               </>
             )}
-         </Popup>
+          </Popup>
         )
       })}
       {/* <DailyScore />
