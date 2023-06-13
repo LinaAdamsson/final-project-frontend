e151a6d3406fb103dd9efb7fa86c2ac1e9b4e174
andrea hedström <andrea.hedstrom@outlook.com>
2023-06-13 15:05:09 +0200

    new avatar, intropage styling

--- src/components/IntroPage.js
+++ src/components/IntroPage.js
@@ -4,7 +4,7 @@ import RegisterForm from 'components/RegisterForm';
 import { Avatar } from 'styles/AvatarStyle';
 import { Planet } from 'styles/PlanetStyle';
 // import Loader from 'components/Loader';
-import { IntroText } from 'styles/StartPage';
+import { IntroText, LoginRegisterWrapper, IntroImage } from 'styles/StartPage';
 import { Button } from 'styles/FormStyle';
 
 const IntroPage = () => {
@@ -29,27 +29,34 @@ const IntroPage = () => {
   return (
     <div>
       {/* <Loader /> */}
+      <IntroImage>
+      <Avatar src="/Images/Hero.png" alt="avatar" />
       <Planet src="https://lh3.googleusercontent.com/U4ss0tLqB8G_bGeYhbAOewyGW25VrveeWXKsdHPzTGa_QD6rTlZcBHjQ1jH01MyvKsKn_c2YluGgLZaCsUgMWD1NaI8Zv1TV9U2mMHPvghWNljflFNu3xTqZ-4R0tawUvCpBGKTZ2A=w2400" alt="planet" />
-      <Avatar src="https://lh3.googleusercontent.com/_0iKG-ssXTtP7Xei8wHoAQndbKvbr9lxtWw97-6AmN6lOFJ-vUj7U3B90JTCGXWOVOxx7XUYfX2RlyHKBh-DwcFgKMFS0rk6tECTrKAmTuypssGcGwd2_XbBcNK-2C4QxLJQ8z85Xg=w2400" alt="avatar" />
       {/* <Avatar src="https://lh3.googleusercontent.com/fd4G2VpauOVpTMbQrQPAuXDkSGSXhaFDMT3-gyAzFuJCP_sPOthfsiOUDPMWOgsbG2D764fIhCvfVPJpksHMqDLcE4BWKhPOhu8C8mTdT-PeR1y5hc3rLd4DXrgFVHXRQqpnIxrnRA=w2400" alt="avatar" /> */}
+      </IntroImage>
       <IntroText>
-        <h2>TIME TO HELP THE PLANET</h2>
+        <h2>TODAYS MISSION:</h2>
+        <h2>Time to help the planet!</h2>
       </IntroText>
       {showLoginForm ? (
         <>
           <LoginForm onSuccess={handleLogin} />
-          <p>
-                New user?{' '}
+          <LoginRegisterWrapper>
+            <p>
+                Ready to accept the challenge?{' '}
+            </p>
             <Button type="button" onClick={handleToggleForm}>Register</Button>
-          </p>
+          </LoginRegisterWrapper>
         </>
       ) : (
         <>
           <RegisterForm onSuccess={handleLogin} />
-          <p>
-                Already have an account?
+          <LoginRegisterWrapper>
+            <p>
+                Already accepted the challenge?
+            </p>
             <Button type="button" onClick={handleToggleForm}>Log in here</Button>
-          </p>
+          </LoginRegisterWrapper>
         </>
       )}
     </div>
--- src/styles/StartPage.js
+++ src/styles/StartPage.js
@@ -1,11 +1,29 @@
 import styled from 'styled-components/macro';
 
+export const IntroImage = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
 export const IntroText = styled.div`
 display: flex;
 justify-content: center;
-margin: 50px 0 -110px 0;
+align-items: center;
+flex-direction: column;
+margin: 50px 0 -70px 0;
 h2{
 font-size: 20px;
+margin-bottom: -20px;
 color: white;
+letter-spacing: 4px;
+font-family:'Bagel Fat One', cursive;
+text-shadow: 1px 1px 2px grey;
 }
+`
+
+export const LoginRegisterWrapper = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
 `
