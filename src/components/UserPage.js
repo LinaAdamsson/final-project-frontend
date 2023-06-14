07c4ff19bbefdb3147e73993df010e1321c852ab
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2023-06-14 16:32:11 +0200

    worked on styling of userpage

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Avatar } from 'styles/AvatarStyle';
-import { DailyScoreCard, ScoreCardWrapper, TotalScoreCard, UserPageWrapper } from 'styles/UserPage';
+import { CardText, DailyPoints, ScoreCard, ScoreCardWrapper, TotalPoints, UserAvatar, UserPageWrapper, UserText, UserTitle } from 'styles/UserPage';
 
 const UserPage = () => {
   const myUser = useSelector((store) => store.user)
 
   return (
-    <>
-      <UserPageWrapper>
-      <Avatar src="/Images/Avatar_moln.png" alt="avatar" />
-      <h2>Super-{myUser.firstName}</h2>
-      <h4>Title: Environment Hero</h4>
-      <h4>Real name: {myUser.firstName} {myUser.lastName}</h4>
+    <UserPageWrapper>
+      <UserAvatar src="/Images/Avatar_moln.png" alt="avatar" />
+      <UserTitle>Super-{myUser.firstName}</UserTitle>
+      <UserText>Real name: {myUser.firstName} {myUser.lastName}</UserText>
+      <UserText>Title: Environment Hero</UserText>
+      <UserText>Strength: Endurance</UserText>
       <ScoreCardWrapper>
-        <DailyScoreCard>
-          <h4>Today&apos;s hero score:</h4>
-          <p>{myUser.dailyScore}</p>
-        </DailyScoreCard>
-        <TotalScoreCard>
-          <p>Total hero score:</p>
-          <p>{myUser.totalScore}</p>
-        </TotalScoreCard>
+        <ScoreCard>
+          <CardText>Today&apos;s score:</CardText>
+          <DailyPoints>{myUser.dailyScore} p</DailyPoints>
+          {/* <CardPoints>{myUser.dailyScore}</CardPoints> */}
+        </ScoreCard>
+        <ScoreCard>
+          <CardText>Total score:</CardText>
+          <TotalPoints>{myUser.totalScore} p</TotalPoints>
+          {/* <CardPoints>{myUser.totalScore}</CardPoints> */}
+        </ScoreCard>
       </ScoreCardWrapper>
     </UserPageWrapper>
-    </>
   );
 }
 
--- src/styles/UserPage.js
+++ src/styles/UserPage.js
@@ -6,34 +6,76 @@ export const UserPageWrapper = styled.div`
     flex-wrap: wrap;
     align-items: center;
     justify-content: center;
+    margin-top: 40px;
+`
+
+export const UserAvatar = styled.img`
+  width: 200px;
+`
+
+export const UserTitle = styled.h2`
+    font-size: 38px;
+    font-family: "Bagel Fat One", cursive;
+    color: yellow;
+    margin: 0px 0px 10px 0px;
+`
+
+export const UserText = styled.p`
+    font-size: 24px;
+    font-family: 'Oswald', sans-serif;    
+    font-weight: 500;
+    color: #086695;
+    margin: 5px;
 `
 
 export const ScoreCardWrapper = styled.div`
-    display: flex;
+    display: flex; 
 `
 
-export const DailyScoreCard = styled.div`
-    background-color: #F08A78;
-    width: 80px;
-    height: 100px;
+export const ScoreCard = styled.div`
+    display: flex; 
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #FFFFFF;
+    opacity: 90%;
+    width: 110px;
+    height: 120px;
     border: none;
     border-radius: 10%;
+    font-family: 'Oswald', sans-serif;
     margin: 10px;
     padding: 10px;
-    color: white;
-    text-transform: uppercase;
+    /* text-transform: uppercase; */
     font-size: 14px;
 `
 
-export const TotalScoreCard = styled.div`
-    background-color: #72B686;
-    width: 80px;
-    height: 100px;
-    border: none;
-    border-radius: 10%;
-    margin: 10px;
-    padding: 10px;
-    color: white;
-    text-transform: uppercase;
-    font-size: 14px;
+export const CardText = styled.h3`
+    font-size: 18px;
+    color: #086695;    
+    margin-top: 30px;
+    margin: 0;
 `
+export const DailyPoints = styled.p`
+    color: #2EAB6F;
+    font-size: 28px;
+    font-weight: 600;
+    margin: 5px;
+    text-align: center;
+`
+
+export const TotalPoints = styled.p`
+    color: #F0B560;
+    font-size: 28px;
+    font-weight: 600;
+    margin: 5px;
+    text-align: center;
+`
+
+// export const CardPoints = styled.p`
+//     color: #F0B560;
+//     font-size: 28px;
+//     font-weight: 600;
+//     margin: 5px;
+//     text-align: center;
+// `
