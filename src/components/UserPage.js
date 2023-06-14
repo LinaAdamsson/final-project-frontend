65d85d0983406340c1e399ca8a0069509419d5ac
Lina Adamsson <studio@adamsson.com>
2023-06-14 12:33:16 +0200

    Worked with position.

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -12,7 +12,7 @@ const UserPage = () => {
       <UserPageWrapper>
         <h2>Super-{myUser.firstName}</h2>
         <ul>
-          <li>Your real name: {myUser.firstName} {myUser.lastName}</li>
+          <li>Real name: {myUser.firstName} {myUser.lastName}</li>
           {/* <li>Your email: {myUser.email}</li> */}
           <li>Hero points today: {myUser.dailyScore}</li>
           <li>Total hero score: {myUser.totalScore}</li>
