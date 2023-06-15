5c1ee75db0698be34eb2112a16edafbfd50c6de9
andrea hedström <andrea.hedstrom@outlook.com>
2023-06-15 12:21:00 +0200

    header styling

--- src/styles/HeaderStyle.js
+++ src/styles/HeaderStyle.js
@@ -7,9 +7,38 @@ export const HeaderWrapper = styled.div`
     height: 90px;
     width: 100vw;
 `
-export const HeaderText = styled.h1`
+export const HeaderText = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+h2 {
     font-size: 15px;
+    color: white;
+    padding-top: 15px;
+    font-family: 'Roboto Slab', serif;
+    @media (min-width: 668px){
+      font-size: 19px;
+    }
+    @media (min-width: 1024px){
+     font-size: 22px;
+     padding-top: 40px;
+    }
+}
+h1 {
+    font-size: 20px;
     font-family: 'Bagel Fat One', cursive;
     color: white;
     letter-spacing: 2px;
+    text-shadow: 1px 1px 2px grey;
+    margin-top: -10px;
+    @media (min-width: 668px){
+      font-size: 25px;
+      letter-spacing: 4px;
+    }
+    @media (min-width: 1024px){
+     font-size: 30px;
+     letter-spacing: 6px;
+    }
+}
 `
--- src/styles/MainStyle.js
+++ src/styles/MainStyle.js
@@ -15,12 +15,50 @@ export const MainContainer = styled.div`
 `
 
 export const SignOutButton = styled.button`
-    width: 70px;
+width: 90px; height: 40px;
+background: white;
+opacity: 80%;
+font-family: 'Roboto Slab', serif;
+color: rgb(161, 191, 206);
+font-size: 15px;
+border-radius: 100px;
+-webkit-border-radius: 100px;
+-moz-border-radius: 100px;
+position: relative;
+border-style: none;
+margin: 80px auto 20px;
+
+::after, ::before {
+content: '';
+backround: white;
+position: absolute;
+z-index: -1
+}
+
+::after {
+background: white;
+width: 30px; height: 30px;
+top: -12px; left: 45px;
+border-radius: 100px;
+-webkit-border-radius: 100px;
+-moz-border-radius: 100px;
+}
+
+::before {
+background: white;
+width: 48px; height: 48px;
+top: -18px; right: 26px;
+border-radius: 200px;
+-webkit-border-radius: 200px;
+-moz-border-radius: 200px;
+}
+
+   /* width: 70px;
     height: 70px;
     border-radius: 100%;
     border-style: none;
     background-color: yellow;
-    font-family: 'Roboto Slab', serif;
+    font-family: 'Roboto Slab', serif; */
 `
 
 /* export const LogoutButton = styled(Button)`
